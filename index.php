616711a16796027225011c8e677f0114d8145841
Yuri M <4regs@mail.ru>
2023-08-10 16:12:16 +0300

    Fixes

--- index.php
+++ index.php
@@ -5,10 +5,8 @@ require_once __DIR__."/vendor/autoload.php";
 use App\DB;
 
 $t = new App\DB\UsersQueries(__DIR__. "/conf/user_queries_db_config.json");
-//$arUserQueries = $t->getUserQueries(1);
 $arUsers = $t->getUsers();
-//print_r($arUsers);
-
+//echo '<pre>'.print_r($_GET).'</pre>';
 ;?>
 <!doctype html>
 <html lang="ru">
@@ -16,11 +14,7 @@ $arUsers = $t->getUsers();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>...</title>
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css" integrity="sha512-rt/SrQ4UNIaGfDyEXZtNcyWvQeOq0QLygHluFQcSjaGB04IxWhal71tKuzP6K8eYXYB6vJV4pHkXcmFGGQ1/0w==" crossorigin="anonymous" referrerpolicy="no-referrer">
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.bundle.min.js" integrity="sha512-igl8WEUuas9k5dtnhKqyyld6TzzRjvMqLC79jkgT3z02FvJyHAuUtyemm/P/jYSne1xwFI06ezQxEwweaiV7VA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>-->
-    <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>-->
-    <!--<script src="/js/main.js"></script>-->
 </head>
 <body>
 
@@ -34,7 +28,7 @@ $arUsers = $t->getUsers();
             <h2>Выбор пользователя и запроса</h2>
             <form id="myform" method="post" action="app/FormHandlers.php">
 
-                  <?php //тут должен быть вывод экземпляра вида для списка пользователей
+                <?php //тут должен быть вывод экземпляра вида для списка пользователей
                 if (!empty($arUsers)):?>
                 <div class="form-group col-sm-4">
                     <label for="user">Пользователь</label>
@@ -42,6 +36,15 @@ $arUsers = $t->getUsers();
                         <?php foreach ($arUsers as $user):?>
                             <option value="<?=$user["id"]?>"><?=$user["name"]?></option>
                         <?php endforeach?>
+                        <?/*php Выбор нужного пользователя по нажатии кнопки Назад
+                        if (!empty($_GET["user"])) {$user_id = $_GET["user"]; echo $user_id;}?>
+                        <?php foreach ($arUsers as $user):
+                            if ($user["id"] == $user_id):?>
+                                <option selected value="<?=$user["id"]?>"><?=$user["name"]?></option>
+                            <?php else:?>
+                                <option value="<?=$user["id"]?>"><?=$user["name"]?></option>
+                            <?php endif;?>
+                        <?php endforeach*/?>
                         </select>
                 </div>
                 <?php endif?>
@@ -55,8 +58,6 @@ $arUsers = $t->getUsers();
                     <button id="add" disabled type="submit" class="form-group btn btn-secondary btn-sm" name="action" value="add">Добавить</button>
                     <button id="edit" disabled type="submit" class="form-group btn btn-secondary btn-sm" name="action" value="edit">Редактировать</button>
                     <button id="delete" disabled type="button" class="form-group btn btn-secondary btn-sm" name="action" value="delete">Удалить</button>
-
-                    <button id="test-button" type="button" class="form-group btn btn-secondary btn-sm">TEST</button>
                 </div>
 
             </form>
